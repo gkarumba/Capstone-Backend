c4222c2c095d23bc7bd5d0dbdbd012ee723b5b39
gkarumba <gachegua@gmail.com>
2019-07-27 10:21:26 +0300

    CHange the survey/serializers

--- schoolsurvey/survey/serializers.py
+++ schoolsurvey/survey/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
 from .models import Survey
 from .models import Category
-<<<<<<< HEAD
+
 from .models import Answers, Questionaire
-=======
-from .models import Question, Answers
->>>>>>> cf263849322247aab8b3cd232ec75db274f5cd70
+
 
 
 class SurveySerializer(serializers.HyperlinkedModelSerializer):
@@ -13,7 +11,6 @@ class SurveySerializer(serializers.HyperlinkedModelSerializer):
         model = Survey
         fields = ('id', 'url', 'name', 'description')
 
-<<<<<<< HEAD
 # class QuestionSerializer(serializers.HyperlinkedModelSerializer):
 
 #     class Meta:
@@ -24,7 +21,7 @@ class SurveySerializer(serializers.HyperlinkedModelSerializer):
 class AnswersSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Answers
-        fields = ('id', 'url', 'school', 'answer', 'questionId')
+        fields = ('id', 'school', 'answer', 'category')
 
 class QuestionaireSerializer(serializers.ModelSerializer):
     pages = serializers.JSONField() # change is here
@@ -38,24 +35,4 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'description','questionaire')
-=======
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'url', 'name', 'survey', 'visibility', 'allows_edit',
-                  'identifier', 'description', 'form_builder_json', 'custom_submit_url')
-
-
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Question
-        fields = ('id', 'url', 'title', 'help', 'category')
-
-
-class AnswersSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Answers
-        fields = ('id', 'url', 'answer', 'question')
->>>>>>> cf263849322247aab8b3cd232ec75db274f5cd70
